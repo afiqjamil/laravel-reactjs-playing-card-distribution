7c8e969d9e4b759587ac53105e45bbf5ea20dad6
afiq <afiq@gosempire.com>
2020-04-08 04:43:34 +0800

    update ui

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,48 +8,62 @@
 
     <title>Playing Card Distribution</title>
 
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
+
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
+    <style>
+        body, html {
+            height: 100%;
+        }
+    </style>
 </head>
 
 <body>
-<div class="row">
-    <div class="col-sm-6">
-        <form class="form" id="form">
-            <div class="text-center mb-4">
-                <h1 class="h3 mb-3 font-weight-normal">Playing Card Distribution</h1>
-                <p>Enter number of player below and submit to start card distribution</p>
-            </div>
+<div class="container-fluid h-100">
+    <div class="row h-100">
+        <div class="col-sm-6 col-2 h-100 bg-dark text-white py-2 d-flex align-items-center justify-content-center fixed-top" id="left">
+            <form class="form" id="form">
+                <div class="text-center mb-4">
+                    <h1 class="h3 mb-3 font-weight-normal">Playing Card Distribution</h1>
+                    <p>Enter number of player below and submit to start card distribution</p>
+                </div>
 
-            <div class="form-label-group">
-                <input type="number" id="player" name="player" class="form-control" placeholder="No. Of Player" min="1" required autofocus>
-            </div>
-            <br/>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
-        </form>
+                <div class="form-label-group">
+                    <input type="number" id="player" name="player" class="form-control" placeholder="No. Of Player" min="1" required autofocus>
+                </div>
+                <br/>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+
+                <br/>
+                <div id="error" class="alert alert-danger" role="alert" style="display: none;">
+                    This is a danger alert—check it out!
+                </div>
+            </form>
+        </div>
+        <div class="col-sm-6 invisible col-2"><!--hidden spacer--></div>
+        <div class="col offset-2 offset-sm-6 py-2">
+            <h4>Result</h4>
+            <table class="table" id="result">
+                <thead>
+                <tr>
+                    <th scope="col">Player</th>
+                    <th scope="col">Card</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
     </div>
-    <div class="col-sm-6">
-        <hr/>
+</div>
+<div class="row">
+    <div class="col-sm-4 sticky-top">
 
-        <table class="table" id="result">
-            <thead>
-            <tr>
-                <th scope="col">Player</th>
-                <th scope="col">Card</th>
-            </tr>
-            </thead>
-            <tbody>
-            {{--<tr>--}}
-            {{--    <th scope="row">#1</th>--}}
-            {{--    <td>Mark</td>--}}
-            {{--</tr>--}}
-            {{--<tr>--}}
-            {{--    <th scope="row">#2</th>--}}
-            {{--    <td>Jacob</td>--}}
-            {{--</tr>--}}
-            </tbody>
-        </table>
+    </div>
+    <div class="col-sm-6 table-responsive">
+
     </div>
 </div>
 
@@ -68,15 +82,22 @@
                 {
                     if (response.status)
                     {
+                        $('#error').empty().hide();
                         $('#result tbody').empty();
                         var trHTML = '';
                         $.each(response.result, function (i, item) {
-                            trHTML += '<tr><td>' + item.player + '</td><td>' + item.cards + '</td></tr>';
+                            if (item.cards.length) {
+                                trHTML += '<tr><th scope="row">#' + item.player + '</th><td>' + item.cards + '</td></tr>';
+                            }
+                            else {
+                                trHTML += '<tr class="table-danger"><th scope="row">#' + item.player + '</th><td>' + item.cards + '</td></tr>';
+                            }
+
                         });
                         $('#result').append(trHTML);
                     }
                     else {
-                        alert("Irregularity occurred: " + response.result.player);
+                        $('#error').empty().append("<b>Irregularity occurred:</b><br/>" + response.result.player).show();
                     }
                 }
             });
